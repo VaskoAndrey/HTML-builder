fcffc8c27a9b2ba8f3a902400e2af0a3138055c0
VaskoAndrey <nate20@mail.ru>
2022-11-10 08:36:08 +0300

    fix: fix 'exit' on second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,18 @@
-const {stdin, stdout} = process;
-const fs = require('fs');
-const path = require('path');
-const output = fs.createWriteStream(path.join(__dirname, 'destination.txt'));
+const { stdin, stdout } = process;
+const fs = require("fs");
+const path = require("path");
+const output = fs.createWriteStream(path.join(__dirname, "destination.txt"));
 
-stdout.write('Write text that you want to add at file destination.txt \n');
-stdin.on('data', data => {
-   if(data == 'exite') {
-      process.on('exit', () => {
-         console.log('Bye-bye');
-         process.exit();
-      })
-   }
-   output.write(data);
-})
+stdout.write("Write text that you want to add at file destination.txt \n");
+stdin.on("data", (data) => {
+  if (data.toString().trim().toLowerCase() === "exit") {
+    console.log("Bye-bye");
+    process.exit();
+  }
+  output.write(data);
+});
+
+process.on("SIGINT", () => {
+  console.log("Bye!");
+  process.exit();
+});
